1f9dd60642d25f2d4c206d345091e12f561c42cf
chirol <gamingdogcrl@gmail.com>
2019-05-25 21:32:54 +0900

    2日目

--- post/models.py
+++ post/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    
+    class Meta:
+        db_table = 'post'
+
+    title = models.CharField(max_length=140)
+    date = models.DateField()
+    body = models.TextField()
