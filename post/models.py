b7351692c7048f1936ff058c3bc6cf70761a7995
chirol <gamingdogcrl@gmail.com>
2019-06-14 16:17:29 +0900

    190614

--- post/models.py
+++ post/models.py
@@ -1,8 +1,26 @@
 from django.db import models
 
 # Create your models here.
-class Post(models.Model):
+class Tags(models.Model):
+    tags = models.CharField('タグ名', max_length=140)
 
-    title = models.CharField(max_length=140)
-    date = models.DateField()
-    body = models.TextField()
+    def __str__(self):
+        return self.tags
+
+
+class Category(models.Model):
+    Category = models.CharField('カテゴリ名', max_length=30)
+
+    def __str__(self):
+        return self.Category
+
+
+class Post(models.Model):
+    tags = models.ManyToManyField(Tags, verbose_name='タグ')
+    categories = models.ManyToManyField(Category, verbose_name='カテゴリ')
+    title = models.CharField('タイトル', max_length=140)
+    date = models.DateField('追加日時')
+    body = models.TextField('本文')
+
+    def __str__(self):
+        return self.title
--- post/views.py
+++ post/views.py
@@ -5,4 +5,4 @@ from .models import Post
 
 class PostView(ListView):
     model = Post
-    
+    print(Post.tags)
