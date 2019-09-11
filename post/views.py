955106929036ff0156be44acd8167a11d5e23dad
chirol <gamingdogcrl@gmail.com>
2019-09-11 18:38:29 +0900

    0911

--- post/views.py
+++ post/views.py
@@ -5,4 +5,6 @@ from .models import Post
 
 class PostView(ListView):
     model = Post
+    queryset = Post.objects.order_by('-date') # クエリセットを日付（降順）に
+    paginate_by = 10 # ページネーション設定
     print(Post.tags)
